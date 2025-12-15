192ab7670545bd1588029a995ea1f35145a25017
Asim Momin <asimmomin.dev@gmail.com>
2025-12-15 22:23:33 +0530

    learning var, const, datatypes

---
+++ 1-var-const.js
@@ -0,0 +1,34 @@
+// for declaring constant we use const keyword
+const accountID = 1232343;
+
+// let for variable declaring
+let accountEmail = "hk@google.com";
+
+// var for variable declaring 
+//! We dont use var because it does not support Block Scope & Function Scope -- to solve this we introduce let keyword
+var accountPassword = "23434";
+
+// js some unexpected behaviours 
+//! It work but Not recommended
+accountCity = "Jaipur";
+
+// declaration with value initialization
+let accountState;
+
+
+//* Changing values trial
+
+// accountID = 321;
+// console.log(accountID)
+
+accountEmail = "vc@km.uk";
+accountPassword = "456";
+accountCity = "Pune";
+
+
+// Amazing keyword for log
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
