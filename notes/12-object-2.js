0a00b555a423b15c8a9ec3a62364979e2f04d823
Asim Momin <asimmomin.dev@gmail.com>
2026-01-01 23:44:34 +0530

    Learn de-structuring and API JSON resposne data

--- notes/12-object-2.js
+++ notes/12-object-2.js
@@ -88,10 +88,105 @@ console.log(Object.entries(tinderUser));
   
 */
 
-
 // Some More Methods of Objects
 
 // hasOwnProperty
 console.log(tinderUser.hasOwnProperty("country")); // => true
 
+//* De-structering of Objects
 
+const course = {
+  name: "React JS Begineer",
+  price: "999",
+  InstructorName: "Singh",
+};
+
+// destructuring
+const { name, price } = course;
+console.log(name);
+console.log(price);
+
+// destructuring with different names
+const { InstructorName: instrcutor } = course;
+console.log(instrcutor);
+
+
+//* This destructuring concept is used in React Props object also 
+
+
+//* JSON objects - API calls
+// Tip - use JSON Formatter Tool to understand Massive Data come from API response
+// Link - https://jsonformatter.org/
+
+/*
+
+* EXAMPLE
+
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+
+*/
